700f4f7a5a7978638534dc73ada38f1022ebbeb6
becastal <bernardo.castaldelli@gmail.com>
2024-10-03 22:03:36 -0300

    primeiro resolvedor

--- labirintos.c
+++ labirintos.c
@@ -3,6 +3,14 @@
 #include <time.h>
 
 // TODO: talvez visualizar a construcao de cada algoritmo? seria mt maneiro.
+// TODO: talvez visualizar o desenvolvimento do caminho.
+// TODO: ver como srand funciona. se pa que vale fazer srand antes de todo algoritmo.
+// TODO: 
+
+int dx2[] = {-2, 2, 0, 0};
+int dy2[] = {0, 0, -2, 2};
+int dx1[] = {-1, 1, 0, 0};
+int dy1[] = {0, 0, -1, 1};
 
 void gera_labirinto() {
 	srand(time(NULL));
@@ -10,8 +18,7 @@ void gera_labirinto() {
 	puts("[+] novo labirinto!");
 	labirinto novo_labirinto;
 
-	int ok_linhas = 0;	
-	while (!ok_linhas) {
+	int ok_linhas = 0;	while (!ok_linhas) {
 		printf("[?] indique a quantidade de linhas: ");
 		scanf("%d", &novo_labirinto.linhas);
 
@@ -43,9 +50,9 @@ void gera_labirinto() {
 	puts("[0] algoritmo da binary tree.");
 	puts("[1] algoritmo de sinewinder.");
 	puts("[2] algoritmo de aldous-border.");
-	puts("[3] algoritmo de wilson.");
-	puts("[4] algoritmo de hunt-and-kill.");
-	puts("[5] algoritmo de recursive backtracking.");
+	//puts("[3] algoritmo de wilson.");
+	//puts("[4] algoritmo de hunt-and-kill.");
+	//puts("[5] algoritmo de recursive backtracking.");
 
 	int ok_algoritmo = 0;
 	while (!ok_algoritmo) {
@@ -64,6 +71,9 @@ void gera_labirinto() {
 			case 2:
 				algoritmo_aldous_border(&novo_labirinto);
 				break;
+			case 3:
+				algoritmo_wilson(&novo_labirinto);
+				break;
 			default:
 				puts("[e] selecao invalida!");
 				ok_algoritmo = 0;
@@ -71,12 +81,22 @@ void gera_labirinto() {
 	}
 
 	printa_labirinto(novo_labirinto);
-	printf("[?] salvar este labirinto em arquivo (s/n)? ");
+
+	resolve_dfs(&novo_labirinto);
 
 	for (int i = 0; i < novo_labirinto.linhas; i++) {
 		free(novo_labirinto.celulas[i]);
 	}
 	free(novo_labirinto.celulas);
+}
+
+int posicao_valida(labirinto* L, int i, int j) {
+    return i >= 1 && j >= 1 && i < L->linhas - 1 && j < L->colunas - 1;
+}
+
+int posicao_aleatoria(labirinto* L, int tipo) {
+    int min = 1, max = (tipo ? L->linhas - 2 : L->colunas - 2);
+    return min + 2 * (rand() % ((max - min) / 2 + 1));
 }
 
 void printa_labirinto(labirinto L) {
@@ -102,7 +122,9 @@ void algoritmo_binary_tree(labirinto* L) {
 
 			if (libera_direita) {
 				L->celulas[posicao_linha][posicao_coluna + 1] = ' ';
-			} else { L->celulas[posicao_linha + 1][posicao_coluna] = ' '; }
+			} else {
+				L->celulas[posicao_linha + 1][posicao_coluna] = ' ';
+			}
 		}
 	}
 }
@@ -133,11 +155,13 @@ void algoritmo_sidewinder(labirinto* L) {
         }
     }
 }
+
 void algoritmo_aldous_border(labirinto* L) {
 	// pra uma posicao aleatoria, seguir escolhendo movimentos aleatorios ate ter visitado todos os vertices.
 	// se voce entrar em uma posicao nao antes visitada, voce liga as duas celulas cortando a parede, se nao,
 	// voce so segue. bizarro tentar achar a complexidade disso. existe um mundo que esse algoritmo nunca acaba;
-	// complexidade: O(sei la * ruim);
+	// complexidade: O(sei_la * ruim);
+	// na real a complexidade media fica ~O(nmlogn);
 
     int contador_visitados = 0;
     
@@ -152,20 +176,17 @@ void algoritmo_aldous_border(labirinto* L) {
         }
     }
 
-    int dx[] = {-2, 2, 0, 0};
-    int dy[] = {0, 0, -2, 2};
-
-    int posicao_linha = 1, posicao_coluna = 1;
+    int posicao_linha = posicao_aleatoria(L, 1);
+    int posicao_coluna = posicao_aleatoria(L, 0);
     contador_visitados--;
     visitado[posicao_linha][posicao_coluna] = 1;
 
     while (contador_visitados > 0) {
         int movimento = rand() % 4;
-        int nova_posicao_linha = posicao_linha + dx[movimento];
-        int nova_posicao_coluna = posicao_coluna + dy[movimento];
+        int nova_posicao_linha = posicao_linha + dx2[movimento];
+        int nova_posicao_coluna = posicao_coluna + dy2[movimento];
 
-        if (nova_posicao_linha <= 0 || nova_posicao_coluna <= 0 ||
-            nova_posicao_linha >= L->linhas - 1 || nova_posicao_coluna >= L->colunas - 1) {
+        if (!posicao_valida(L, nova_posicao_linha, nova_posicao_coluna)) {
             continue;
         }
 
@@ -185,3 +206,60 @@ void algoritmo_aldous_border(labirinto* L) {
     free(visitado);
 }
 
+void algoritmo_wilson(labirinto* L) {
+
+}
+
+void dfs(int** distancia, labirinto* L, int linha, int coluna) {
+	for (int i = 0; i < 4; i++) {
+		int nova_linha = linha + dx1[i], nova_coluna = coluna + dy1[i];
+		if (posicao_valida(L, nova_linha, nova_coluna) && 
+			distancia[nova_linha][nova_coluna] == -1   &&
+			L->celulas[nova_linha][nova_coluna] != '#') {
+			distancia[nova_linha][nova_coluna] = distancia[linha][coluna] + 1;
+			dfs(distancia, L, nova_linha, nova_coluna);
+		}
+	}
+}
+
+void resolve_dfs(labirinto* L) {
+	int** distancia = (int**) malloc(L->linhas * sizeof(int*));
+	for (int i = 0; i < L->linhas; i++) {
+		distancia[i] = (int*) malloc(L->colunas * sizeof(int));
+		for (int j = 0; j < L->colunas; j++) {
+			distancia[i][j] = -1;
+		}
+	}
+
+	distancia[1][1] = 0;
+	dfs(distancia, L, 1, 1);
+
+	char** celulas_resolvido = L->celulas;
+	int posicao_linha = L->linhas - 2, posicao_coluna = L->colunas - 2;
+	while (distancia[posicao_linha][posicao_coluna] >= 0) {
+		celulas_resolvido[posicao_linha][posicao_coluna] = '*';
+		
+		for (int i = 0; i < 4; i++) {
+			int nova_posicao_linha = posicao_linha + dx1[i], nova_posicao_coluna = posicao_coluna + dy1[i];
+			if (distancia[nova_posicao_linha][nova_posicao_coluna] + 1 == distancia[posicao_linha][posicao_coluna]) {
+
+				posicao_linha = nova_posicao_linha;
+				posicao_coluna = nova_posicao_coluna;
+				break;
+			}
+		}
+	}
+
+	for (int i = 0; i < L-> linhas; i++) {
+		for (int j = 0; j < L->colunas; j++) {
+			printf("%s", (celulas_resolvido[i][j] == '*' ? "\033[1;31m" : "\033[0m"));
+			printf("%c", celulas_resolvido[i][j]);
+		}
+		puts("");
+	}
+
+    for (int i = 0; i < L->linhas; i++) {
+        free(distancia[i]);
+    }
+    free(distancia);
+}
--- labirintos.h
+++ labirintos.h
@@ -2,17 +2,20 @@
 #define labirintos_h
 #include <stdio.h>
 
-typedef struct labirinto labirinto;
-struct labirinto {
+typedef struct {
 	int linhas, colunas;
 	char** celulas;
-};
+} labirinto;
 
 void gera_labirinto();
 int posicao_valida(labirinto* L, int i, int j);
+int posicao_aleatoria(labirinto* L, int tipo);
 void printa_labirinto(labirinto L);
 void algoritmo_binary_tree(labirinto* L);
 void algoritmo_sidewinder(labirinto* L);
 void algoritmo_aldous_border(labirinto* L);
+void algoritmo_wilson(labirinto* L);
+void dfs(int** distancia, labirinto* L, int linha, int coluna);
+void resolve_dfs(labirinto* L);
 
 #endif
