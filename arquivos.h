bc8b418fcecc3c360ab6a77f8f10262f85873c9a
Gabriel Engle <gab.engle.dev@gmail.com>
2024-10-11 14:51:31 -0300

    Esqueleto de salvar em binário as pessoas e seus respectivos pontos

--- arquivos.h
+++ arquivos.h
@@ -5,15 +5,21 @@
 
 void salvar_arquivo(labirinto L){
     FILE *arq = fopen("mapas/mapas.txt", "w");
-	fprintf(arq, "%d %d %c", L.linhas,L.colunas,L.celulas);
+	fprintf(arq, "%d\n%d\n%c", L.linhas,L.colunas,L.celulas);
 	fclose(arq);
 }
 
 void salvar_bin(labirinto L){
-    FILE *arq = fopen("mapas/mapas.txt", "wb");
+    FILE *arq = fopen("mapas/mapas.bin", "wb");
     fwrite(L.linhas, sizeof(int),1,arq);
     fwrite(L.colunas, sizeof(int),1,arq);
     fwrite(L.celulas, sizeof(int),1,arq);
+}
+
+void usuario_bin(labirinto L){
+    arq = fopen("mapas/mapas.bin", "wb");
+	fwrite(nome, sizeof(char[]),1,arq);
+    fwrite(pontuacao, sizeof(int),1,arq);
 }
 
 #endif
