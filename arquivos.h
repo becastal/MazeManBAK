d8a319d0ea5bcf057b2f307b04d15e4fa6c65898
Gabriel Engle <gab.engle.dev@gmail.com>
2024-10-11 16:10:49 -0300

    Atualização de salvar labirintos em .bin e .txt e o esqueleto de salvar a pontuação e o nome dos jogadores

--- arquivos.h
+++ arquivos.h
@@ -3,23 +3,37 @@
 #include <stdio.h>
 #include <conio.h>
 
+void salvar_bin(labirinto L){
+    FILE *arq = fopen("mapas/mapas.bin", "wb");
+    fwrite(L.linhas, sizeof(int),1,arq);
+    fwrite(L.colunas, sizeof(int),1,arq);
+    fwrite(L.celulas, sizeof(celulas[][]),1,arq);
+}
+
+void ler_bin(labirinto L){
+	arq = fopen("mapaa/mapas.bin", "rb");
+    fread(L.linhas, sizeof(int),1,arq);
+    fread(L.colunas, sizeof(int),1,arq);
+    fread(L.celulas, sizeof(celulas[][]),1,arq);
+}
+
 void salvar_arquivo(labirinto L){
     FILE *arq = fopen("mapas/mapas.txt", "w");
 	fprintf(arq, "%d\n%d\n%c", L.linhas,L.colunas,L.celulas);
 	fclose(arq);
 }
 
-void salvar_bin(labirinto L){
-    FILE *arq = fopen("mapas/mapas.bin", "wb");
-    fwrite(L.linhas, sizeof(int),1,arq);
-    fwrite(L.colunas, sizeof(int),1,arq);
-    fwrite(L.celulas, sizeof(int),1,arq);
+void usuario_txt(labirinto L){
+	FILE *arq = fopen("mapas/usuarios.txt", "w");
+	fprintf(arq, "%c\n%d", nome[],pontuacao);
+	fclose(arq);
 }
 
-void usuario_bin(labirinto L){
-    arq = fopen("mapas/mapas.bin", "wb");
-	fwrite(nome, sizeof(char[]),1,arq);
-    fwrite(pontuacao, sizeof(int),1,arq);
+void ler_usuarios(labirinto L){
+	arq = fopen("mapas/usuarios.txt", "r");
+	fgets(nome,sizeof(nome[]),arq);
+	fgets(pontuacao,sizeof(int),arq);
+	fclose(arq);
 }
 
 #endif
