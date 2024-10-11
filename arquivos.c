45459ec259d599cd140f69baff6125c98d530f10
Gabriel Engle <gab.engle.dev@gmail.com>
2024-10-11 14:55:56 -0300

    adição de um prototipo de função

--- arquivos.c
+++ arquivos.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "arquivos.h"
 
-void salvar_arquivo(labirinto L)
-void salvar_bin(labirinto L)
+void salvar_arquivo(labirinto L);
+void salvar_bin(labirinto L);
+void usuario_bin(labirinto L);
